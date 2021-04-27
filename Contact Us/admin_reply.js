043a0d25604c5e00f740fd2f2c478ecfd08dd4d4
YS4100 <yashvi4100@gmail.com>
2021-04-27 21:48:18 +0530

    Final

--- Contact Us/admin_reply.js
+++ Contact Us/admin_reply.js
@@ -80,7 +80,7 @@ function sendEmail(){
               resolved:resolved
             })
           });
-          
+          window.location.replace('admin_complaint.html');
         });
   });
   });
